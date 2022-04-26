c066ca279ded7a9a23ce1828c79d2e2fdcfc68b6
IGP <pyltsov@gmail.com>
2022-04-26 10:38:09 +0200

    Initial Commit

---
+++ AppricotTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppricotTest
+//
+//  Created by Igor Pyltsov on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
